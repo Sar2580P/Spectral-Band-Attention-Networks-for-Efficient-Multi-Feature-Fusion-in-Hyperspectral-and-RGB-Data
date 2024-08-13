22ad3e75f1ba04ec1500cf345a567891e6968e67
Sar2580P <sporwal1818@gmail.com>
2024-08-14 00:13:46 +0530

    created poetry env

--- activate.sh
+++ activate.sh
@@ -1,5 +1,20 @@
+echo "Jai Shree Ram"
+user_name=$(whoami)
+path_to_env="/home/${user_name}/.cache/pypoetry/virtualenvs/wheat-seed-classification-*"
+if [ -d $path_to_env ]; then
+  echo "Found the existing environment!!"
+else
+  echo "Creating a working environment ..."
+  poetry install --no-root
+
+fi
+echo -e "\n\nActivating the working environment..."
+
+
+source .env
+wandb login --relogin "$WANDB_API"
+
 chmod +x models/hsi/schedule_run.sh
 chmod +x models/rgb/schedule_run.sh
 
-source env/bin/activate
-
+poetry shell
